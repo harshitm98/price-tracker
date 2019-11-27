b48ae805dbc5eb000c6683d1c0ded6aae7896f5d
Harshit Maheshwari <harshitmah98@gmail.com>
2019-11-28 00:22:07 +0530

    Refactors FlipkartScrapper

--- flipkart_scrapper.py
+++ flipkart_scrapper.py
@@ -19,50 +19,66 @@ class FlipkartScrapper:
     def __init__(self, url):
         print('Running Flipkart scrapper...')
 
-    def get_title_price_image_ratings(self, retry_count=0):
-        resultPage = requests.get(url, headers=self.headers)
-        if resultPage.status_code == 200:
-            tree = html.fromstring(resultPage.content)
-            title = ""
-            # Title
-            for elem in tree.xpath('//span[@class="_35KyD6"]/text()'):
-                title += elem
-            # Price
-            for elem in tree.xpath('//div[@class="_1vC4OE _3qQ9m1"]/text()'):
-                price = elem
-            # Images
-            image_urls = []
-            for elem in tree.xpath('//div[@class="_2_AcLJ"]/@style'):
+    def get_title(self, tree):
+        title = ""
+        for elem in tree.xpath('//span[@class="_35KyD6"]/text()'):
+            title += elem
+        return title
+
+    def get_price(self, tree):
+        for elem in tree.xpath('//div[@class="_1vC4OE _3qQ9m1"]/text()'):
+            price = elem
+        return price
+
+    def get_image(self, tree):
+        image_urls = []
+        for elem in tree.xpath('//div[@class="_2_AcLJ"]/@style'):
+            elem = str(elem)
+            if elem.find("url(") != -1:
+                image_url = elem[elem.find("url(") + 4:elem.find(
+                    ")", elem.find("url(") + 4)]
+                image_urls.append(image_url)
+        if len(image_urls) == 0:
+            for elem in tree.xpath('//div[@class="_2_AcLJ _3_yGjX"]/@style'):
                 elem = str(elem)
                 if elem.find("url(") != -1:
                     image_url = elem[elem.find("url(") + 4:elem.find(
                         ")", elem.find("url(") + 4)]
                     image_urls.append(image_url)
-            if len(image_urls) == 0:
-                for elem in tree.xpath('//div[@class="_2_AcLJ _3_yGjX"]/@style'):
-                    elem = str(elem)
-                    if elem.find("url(") != -1:
-                        image_url = elem[elem.find("url(") + 4:elem.find(
-                            ")", elem.find("url(") + 4)]
-                        image_urls.append(image_url)
-            # Ratings
-            ratings = ""
-            for elem in tree.xpath('//div[@class="hGSR34"]'):
-                 ratings = elem.text
-            if ratings == "":
-                for elem in tree.xpath('//div[@class="hGSR34 bqXGTW"]'):
-                    ratings = elem.text
+        return image_urls[0]
 
-            # Offers
-            offers = []
-            for elem in tree.xpath('//li["_2-n-Lg col"]/span'):
-                # To avoid <span class="_2YJn2R">Bank Offer</span>
-                if elem.get("class") != "_2YJn2R":
-                    etree.strip_tags(elem,'strong')
-                    offers.append(elem.text)
-                    
-            print("Title: {}\tPrice: {}\tImage Url: {}\tNumber of Images: {}\tRating: {}\tOffers: {}".format(
-                title, price, image_urls[0], len(image_urls), ratings, offers))
+    def get_ratings(self, tree):
+        ratings = ""
+        for elem in tree.xpath('//div[@class="hGSR34"]'):
+            ratings = elem.text
+        if ratings == "":
+            for elem in tree.xpath('//div[@class="hGSR34 bqXGTW"]'):
+                ratings = elem.text
+        return ratings
+
+    def get_offers_list(self, tree):
+        offers = []
+        for elem in tree.xpath('//li["_2-n-Lg col"]/span'):
+            # To avoid <span class="_2YJn2R">Bank Offer</span>
+            if elem.get("class") != "_2YJn2R":
+                etree.strip_tags(elem, 'strong')
+                offers.append(elem.text)
+        return offers
+
+    def get_details(self, tree):
+        title = self.get_title(tree)
+        price = self.get_price(tree)
+        image_url = self.get_image(tree)
+        ratings = self.get_ratings(tree)
+        offers = self.get_offers_list(tree)
+
+        print("Title: {}\nPrice: {}\nImage Url: {}\nRating: {}\nOffers: {}\n\n".format(
+            title, price, image_url, ratings, offers))
+
+    def load_url(self, retry_count=0):
+        resultPage = requests.get(url, headers=self.headers)
+        if resultPage.status_code == 200:
+            self.get_details(html.fromstring(resultPage.content))
 
 
 if __name__ == '__main__':
@@ -72,4 +88,4 @@ if __name__ == '__main__':
     urls.append('https://www.flipkart.com/redmi-note-7s-astro-moonlight-white-32-gb/p/itm5e8bac3bcf4c4?pid=MOBFJFZDP9R3Z7YK&lid=LSTMOBFJFZDP9R3Z7YKQJSH2X&marketplace=FLIPKART&sattr[]=color&sattr[]=storage&sattr[]=ram&st=ram&otracker=hp_omu_Top%252BOffers_2_3.dealCard.OMU_88559FM66S42_3')
 
     for url in urls:
-        FlipkartScrapper(url).get_title_price_image_ratings()
+        FlipkartScrapper(url).load_url()
